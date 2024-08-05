10fe7b3da3b6ef8a13c58b5512910c1b44d8f133
ccbikai <ccbikai@gmail.com>
2024-08-05 12:04:34 +0800

    feat: add sitemap generation for SEO enhancement
    Implements dynamic sitemap generation to improve SEO by providing search engines with updated site structure and content availability. This includes a root sitemap index and paginated sitemap files for individual posts, enhancing crawl efficiency and content discoverability.

---
+++ src/pages/sitemap/[cursor].xml.js
@@ -0,0 +1,28 @@
+import { getChannelInfo } from '../../lib/telegram'
+
+export const prerender = false
+
+export async function GET(Astro) {
+  const request = Astro.request
+  const url = new URL(request.url)
+  const channel = await getChannelInfo(Astro, {
+    before: Astro.params.cursor,
+  })
+  const posts = channel.posts || []
+
+  const xmlUrls = posts.map(post => `
+    <url>
+      <loc>${url.origin}/posts/${post.id}</loc>
+      <lastmod>${new Date(post.datetime).toISOString()}</lastmod>
+    </url>
+  `).join('')
+
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${xmlUrls}
+</urlset>`, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+}
