dac74bc9d86a302d77f640c6e3b8d9c8e0120a56
ccbikai <ccbikai@gmail.com>
2024-09-18 20:52:59 +0800

    chore: conditionally enable Sentry integration based on environment variable
    Conditionally enables Sentry integration only if the SENTRY_DSN environment variable is set, improving build efficiency and reducing unnecessary dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
   output: 'hybrid',
   adapter: providers[adapterProvider] || providers.node,
   integrations: [
-    ...process.env.SENTRY_DSN
+    ...(process.env.SENTRY_DSN
       ? [
           sentry({
             enabled: {
@@ -44,7 +44,7 @@ export default defineConfig({
             },
           }),
         ]
-      : [],
+      : []),
   ],
   vite: {
     ssr: {
