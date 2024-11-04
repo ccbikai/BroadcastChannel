0035b84e2c528a350f8d4652a59ae18424d10571
ccbikai <ccbikai@gmail.com>
2024-11-04 19:56:30 +0800

    feat: enhance middleware to set speculation rules header

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,10 @@
-export function onRequest(context, next) {
+export async function onRequest(context, next) {
   context.locals.SITE_URL = `${import.meta.env.SITE ?? ''}${import.meta.env.BASE_URL}`
-  return next()
+
+  const response = await next()
+
+  if (!response.bodyUsed) {
+    response.headers.set('Speculation-Rules', '"/rules/prefetch.json"')
+  }
+  return response
 };
---
+++ src/pages/rules/prefetch.json.js
@@ -0,0 +1,22 @@
+export const prerender = false
+
+export async function GET() {
+  return Response.json({
+    prerender: [
+      {
+        urls: ['/', '/tags'],
+        eagerness: 'eager',
+      },
+    ],
+    prefetch: [
+      {
+        where: { href_matches: ['/posts/*'] },
+        eagerness: 'moderate',
+      },
+    ],
+  }, {
+    headers: {
+      'Content-Type': 'application/speculationrules+json',
+    },
+  })
+}
