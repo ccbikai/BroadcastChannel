9b5f9c4a1e86e7caed5d61585b23e53a86ed90c9
56kFM <168373594+56kFM@users.noreply.github.com>
2025-10-11 21:54:54 -0700

    Revert "Implement small dark embeds for major media providers"

--- src/middleware.js
+++ src/middleware.js
@@ -66,11 +66,14 @@ export async function onRequest(context, next) {
 
   const cspDirectives = [
     'default-src \'self\'',
-    'script-src \'self\'',
+    'script-src \'none\'',
     'style-src \'self\' \'unsafe-inline\'',
-    'img-src \'self\' https://i.ytimg.com data:',
-    'frame-src https://www.youtube-nocookie.com https://bandcamp.com https://w.soundcloud.com https://open.spotify.com https://embed.music.apple.com',
+    'img-src \'self\' https: data:',
+    'media-src \'self\' https:',
+    'frame-src \'self\' https: https://www.youtube-nocookie.com https://www.youtube.com https://open.spotify.com https://embed.music.apple.com https://w.soundcloud.com https://bandcamp.com',
+    'connect-src \'self\' https:',
     'frame-ancestors \'none\'',
+    'upgrade-insecure-requests',
   ]
 
   response.headers.set('Content-Security-Policy', cspDirectives.join('; '))
